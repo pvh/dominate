06c3087ed93b26adc3d3c5258dabdf5435b81753
Peter van Hardenberg <pvh@heroku.com>
2011-03-06 15:18:18 -0800

    hajime

---
+++ dominate.cs
@@ -0,0 +1,13 @@
+// Namespace Declaration
+using System;
+
+// Program start class
+class Application
+{
+    // Main begins program execution.
+    static void Main()
+    {
+        // Write to console
+        Console.WriteLine("Commence Domination"); 
+    }
+}
